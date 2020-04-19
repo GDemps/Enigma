bdeea9915f8b48dae7212781a343b4b5f83ab0c5
Grant Dempsey <CryptoDemps@gmail.com>
2020-04-18 20:46:20 -0600

    Encryption Setup

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,30 @@
 class Enigma
 
-  def initialize
+  attr_reader :alphabet
 
+  def initialize
+    @alphabet = ("a".."z").to_a << " "
   end
 
   # puts "How many shifts?"
   # shift = gets.chomp.to_i
 
   def encrypt(message, key, date)
-    @alphabet = ("a".."z").to_a << " "
+    message = "hello world"
+    key = 4.times.map { (0..9).to_a.sample }.join.rjust(5, "0")
+    a_key = key[0..1]
+    b_key = key[1..2]
+    c_key = key[2..3]
+    d_key = key[3..4]
+    date = Time.now.strftime("%d%m%y").to_i
+    last4 = (date * date).to_s[-4..-1]
+    a_offset = last4[0]
+    b_offset = last4[1]
+    c_offset = last4[2]
+    d_offset = last4[3]
+
+    require "pry"
+    binding.pry
   end
 
 end
--- test/test_enigma.rb
+++ test/test_enigma.rb
@@ -11,12 +11,17 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma1
   end
 
+  def test_alphabet
+    enigma1 = Enigma.new
+    assert_equal enigma1.alphabet, ("a".."z").to_a << " "
+  end
+
   def test_encrypt
     enigma1 = Enigma.new
     expected = { encryption: "keder ohulw",
                  key: "02715",
                  date: "040895" }
-    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+    assert_equal expected, enigma1.encrypt("hello world", "02715", "040895")
   end
 
 
