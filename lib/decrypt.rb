839a226165ed3e2c1c65c7a4804c50f4373d9bc8
Grant Dempsey <CryptoDemps@gmail.com>
2020-04-21 08:04:31 -0600

    repeater

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -7,9 +7,7 @@
 # used for decryption. In addition to writing the decrypted message to the file,
 # your program should output to the screen the file it wrote to, the key used
 # for decryption, and the date used for decryption.
-#
 # You should be able to use your CLI like this:
-#
 # $ ruby ./lib/encrypt.rb message.txt encrypted.txt
 # Created 'encrypted.txt' with the key 82648 and date 240818
 # $ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
